ae2d70537e3b250edd9d2a753fab776bf070ffe7
alexmzirai <alexmzirai@gmail.com>
2018-01-31 21:42:29 +0300

    made an update using urllib3 instead of urllib2

--- ExchangeRate.py
+++ ExchangeRate.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import os
 import json
-import urllib2
+import urllib3
 from bs4 import BeautifulSoup
 
 
@@ -11,7 +11,7 @@ app = Flask(__name__, template_folder='tmpl_dir')
 
 
 def get_exchange_rates():
-    rates = []  # rate initially set to an empty list
+    '''rates = []  # rate initially set to an empty list
     response = urllib2.urlopen('http://api.bitcoincharts.com/v1/weighted_prices.json')  # fetch the data using urllib2
     data = response.read()     # read the data received from the response
     rdata = json.loads(data, parse_float=float)  # parse the json data from the url
@@ -20,29 +20,29 @@ def get_exchange_rates():
     rates.append(rdata['rates']['GBP'])
     rates.append(rdata['rates']['JPY'])
     rates.append(rdata['rates']['AUD'])
+    return rates'''
+
+    rates = []
+    url = 'http://api.bitcoincharts.com/v1/weighted_prices.json'
+    http = urllib3.PoolManager()
+    response = http.request('GET', url)
+    # soup = BeautifulSoup(response.data.decode('utf-8'))
+    data = response.read()
+    rdata = json.loads(data, parse_float=float)
+
+    rates.append(rdata['rates']['USD'])
+    rates.append(rdata['rates']['GBP'])
+    rates.append(rdata['rates']['JPY'])
+    rates.append(rdata['rates']['AUD'])
     return rates
 
-    # rates = []
-    # url = 'http://api.bitcoincharts.com/v1/weighted_prices.json'
-    # http = urllib3.PoolManager()
-    # response = http.request('GET', url)
-    # soup = BeautifulSoup(response.data.decode('utf-8'))
-    # data = response.read()
-    # rdata = json.loads(data, parse_float=float)
-
-    # rates.append(rdata['rates']['USD'])
-    # rates.append(rdata['rates']['GBP'])
-    # rates.append(rdata['rates']['JPY'])
-    # rates.append(rdata['rates']['AUD'])
-
-
-rates = get_exchange_rates()
-print(rates)
+    # rates = get_exchange_rates()
 
 
 @app.route('/')
 def index():
     rates = get_exchange_rates()
+    print(rates)
     return render_template('test.html', **locals())
 
 
